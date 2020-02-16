68fa8b6aa6ecbc2dff6fcf7b017f5f5caad3a11d
Yasmina MEKKI <yas.mekki@gmail.com>
2020-02-16 17:43:52 +0100

    karyotype plot + bloc distribution plot

--- R/plotting.R
+++ R/plotting.R
@@ -19,24 +19,65 @@ phenotype_distribution <- function(phenotypes_path){
 
 
 
+
 #' haplotype block distribution
 #'
-#' @param df_blocs
+#' @param df_blocs blocs
+#' @param xlim range of the x axe
+#' @param ylim range of the y axe
+#' @param alpha alpha
+#' @param per_chromosome if the distribution are grouped by chromosome or not. By default FALSE.
 #'
 #' @return ggplot
-#' @import ggplot2
 #' @export
-haplotype_block_distribution <- function(df_blocs){
+haplotype_block_distribution <- function(df_blocs, xlim=NULL, ylim=NULL, alpha=0.1, per_chromosome=FALSE){
+  factor = df_blocs$delta
+  if(per_chromosome){factor = df_blocs$chr}
 
+  haplotype_block_distribution_plot <- ggplot(df_blocs%>% mutate(lh=to_cm-from_cm), aes(x=lh, fill=factor(factor))) +
+    geom_density(alpha=alpha) + labs(fill = "δ(cM)")
+  if(!is.null(xlim)){haplotype_block_distribution_plot <- haplotype_block_distribution_plot + xlim(xlim[1], xlim[2])}
+  if(!is.null(ylim)){haplotype_block_distribution_plot <- haplotype_block_distribution_plot + ylim(ylim[1], ylim[2])}
+  if(per_chromosome){haplotype_block_distribution_plot <- haplotype_block_distribution_plot + facet_grid(rows=vars(delta), scale="free")}
+
+  return(haplotype_block_distribution_plot)
 }
 
 
-#' karyotype plot
+#' Genome coverage of haplotype blocs
 #'
-#' @return ggplot
+#' @param df_blocs data.frame
+#' @param colors colors
+#' @import karyoploteR
+#' @return karyotype plot object
 #' @export
-karyotype_plot <- function(){
+karyotype_plot <- function(df_blocs, colors=NULL){
+  # TO DO
+  # handle colors params
+  # add scale bar
 
+  # params blocs setting
+  df_blocs$lbp = df_blocs$to_bp - df_blocs$from_bp
+  df_blocs$mb  = round(df_blocs$from_bp/1e6)
+  sum_l_bp_chr = df_blocs %>% group_by(chr, mb) %>% summarise(coverage=sum(lbp)/1e6)
+
+  # params plot setting
+  pp <- getDefaultPlotParams(plot.type = 1)
+  pp$data1height = 200
+
+  # init karyotype object with params plot chosen above
+  karyotype_plot_obj <- plotKaryotype(chromosomes=c("autosomal"), plot.params = pp)
+
+  kpHeatmap(karyoplot = karyotype_plot_obj,
+            chr       = sum_l_bp_chr$chr,
+            x0        = sum_l_bp_chr$mb*1e6,
+            x1        = (sum_l_bp_chr$mb+1)*1e6,
+            y         = round(sum_l_bp_chr$coverage*100),
+            colors    = rainbow(200)[round(sum_l_bp_chr$coverage*100)]) #rainbow(200, start=0.2, end=0.7)
+  # add base numbers
+  kpAddBaseNumbers(karyotype_plot_obj)
+
+  return(karyotype_plot_obj)
 }
 
 
