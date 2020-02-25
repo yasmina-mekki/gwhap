28472e1e427ce70e5b534ec0846862ee00a001f6
Mekki Yasmina Nozha <yas.mekki@gmail.com>
2020-02-25 13:57:44 +0100

    ADD: data/bim file to examplify the update process

--- devtools_history.R
+++ devtools_history.R
@@ -1,4 +1,8 @@
 usethis::use_package("data.table")
 usethis::use_package("readr")
 usethis::use_package("ggplot2")
+usethis::use_package("dplyr")
+usethis::use_package("magrittr")
+usethis::use_package("karyoploteR")
+usethis::use_package("grDevices")
 usethis::use_build_ignore("devtools_history.R")
