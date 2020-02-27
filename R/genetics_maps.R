29660c045a5b9106edba23080022944a49682f1a
Karkar Slim <karkar.slim@gmail.com>
2020-02-27 15:40:51 +0000

    Specific get function for Rutgers get_rutgers_genMap_SexAverage in Map genetics_maps.R

--- R/genetics_maps.R
+++ R/genetics_maps.R
@@ -31,3 +31,19 @@ download_rutgers_map <- function(){
 get_rutgers_map <- function(file_path){
   return(suppressMessages(fread(file_path)))
 }
+
+
+#' get rutgers map in genMap format [bp,cM]
+#'
+#' @param file_path the location of the rutgers map
+#'
+#' @return genetic_map : the genetetic mapin genMap format
+#'
+#' @import data.table
+#'
+#' @export
+get_rutgers_genMap_SexAverage <- function(genetic_map_path, chr){
+  ru_map=(suppressMessages(fread(sprintf('%s/RUMapv3_B137_chr%s.txt', genetic_map_path, chr))))
+  return(data.frame(bp=ru_map$Build37_start_physical_position,
+                    cM=ru_map$Sex_averaged_start_map_position))
+}
