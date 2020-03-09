7af420fdc7bd5e72bf8c93945c303873b1b268f1
Yasmina MEKKI <yas.mekki@gmail.com>
2020-03-10 00:02:23 +0100

    optimise function and change haps names

--- R/determine_haplotypes.R
+++ R/determine_haplotypes.R
@@ -13,52 +13,22 @@
 #' @import DBI
 #' @import dummies
 #' @export
-#'
-determine_haplotypes=function(chr, start, end, bgnfile, samples_index, max_entries_per_sample=4)
+#' 
+determine_haplotypes_per_bloc = function(chr, start, end, bgnfile, samples_index, mysamples, max_entries_per_sample=4)
 {
-  # we should use the ID not their index ...
-  # for the returned object, we should keep the ID as well
-
-  # get the IDs of the partcipants indexes
-  # this part should be done outside of the function
-  # use get_bgi_file and get_bgen_file functions
-  # use the .sample to get both index and ID ?
-
-  bgifile = sprintf("%s.bgi", bgnfile)
-  con = (dbConnect(RSQLite::SQLite(), bgifile))
-  allVar = data.frame(dbReadTable(con, "Variant"))
-  dbDisconnect(con)
-  dummybg = bgen.load(
-    filename = bgnfile,
-    data.frame(
-      chromosome = '',
-      start = allVar$position[1],
-      end = allVar$position[1]
-    ),
-    max_entries_per_sample = max_entries_per_sample
-  )
-
-  # filter the partcipant ID using the their index
-  mysamples = dummybg$samples[samples_index]
-
   # read the bgen file
-  # use the function get_bgen_file ==> mybg = get_bgen_file(bgnfile, start+1, end-1, sample=mysamples, max_entries_per_sample=4)
-  mybg = bgen.load(
-    filename = bgnfile,
-    data.frame(
-      chromosome = '',
-      start =start-1, # to make sure we have the starting position
-      end = end+1 # to make sure we have the ending position
-    ),
-    samples = mysamples,
-    max_entries_per_sample = max_entries_per_sample
-  )
+  mybg = get_bgen_file(file_path = bgnfile,
+                       start = start-1, # to make sure we have the starting position
+                       end = end+1, # to make sure we have the ending position
+                       samples = mysamples,
+                       chromosome = '',
+                       max_entries_per_sample = max_entries_per_sample)
 
   # get genotype probability values, indexed by variants, samples, and by genotype.
   # c(4) and c(2) correspond to the genotype probability fields. they depend on max_entries_per_sample parameters chosen. Justify the value==4.
   allhap_2 = t(mybg$data[, , c(4)])
   allhap_1 = t(mybg$data[, , c(2)])
-  remove(mybg)
+  #remove(mybg)
   colnames(allhap_1) = colnames(allhap_2) = c()
 
   # concatenate and
@@ -72,12 +42,14 @@ determine_haplotypes=function(chr, start, end, bgnfile, samples_index, max_entri
 
   # dummification
   df = dummy.data.frame(data.frame(levels(rlefact)[as.numeric(hapfact)]))
-  my.effect.diplo = df[1:length(mysamples),] + df[(length(mysamples) +1):(2 * length(mysamples)),]
-  colNames=colnames(my.effect.diplo)
-  haps=unlist(lapply(strsplit(colNames,'_'),function(x) x[length(x)]))
-
-  colnames(my.effect.diplo)=apply(data.frame(chr,start, end,haps),1,paste,collapse='_')
-  row.names(my.effect.diplo)=samples_index
+  my.effect.diplo = df[1:length(mysamples), ] + df[(length(mysamples) +1):(2 * length(mysamples)), ]
+  colNames = colnames(my.effect.diplo)
+  haps = unlist(lapply(strsplit(colNames,'_'), function(x) x[length(x)]))
+  
+  # changing haps name format into chr_start_end_haps-code
+  haps = vapply(strsplit(haps,"hapfact.."), `[`, 2, FUN.VALUE=character(1))
+  
+  colnames(my.effect.diplo) = apply(data.frame(chr, start, end, haps), 1, paste, collapse='_')
+  row.names(my.effect.diplo) = samples_index
   return(my.effect.diplo)
 }
-
