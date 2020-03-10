d8b02609bb5368bd97e7911f94d15ada828eeaee
Yasmina MEKKI <yas.mekki@gmail.com>
2020-03-10 18:55:48 +0100

    test for augmented map creation using 1000 genome interpolated

--- test_modules/augmented_map_blocs_test.R
+++ test_modules/augmented_map_blocs_test.R
@@ -1,13 +1,6 @@
-if (!require(devtools)){install.packages("devtools")}
-remove.packages('gwhap')
-devtools::install_github("yasmina-mekki/gwhap", force = TRUE)
-library(gwhap)
-
-
-
 #_____________________________________________________________
 # Part 1
-# Update the genetic map
+# Update the genetic map using the rutgers map
 #_____________________________________________________________
 
 # Update the genetic map
@@ -28,15 +21,18 @@ augmented_genetic_map_dir = '/neurospin/tmp/ymekki/new_repo/gwhap/augmented_gene
 augmented_map_df = get_augmented_genetic_map(augmented_genetic_map_dir, chromosomes=1:23)
 
 
-#_____________________________________________________________
-# Part 1
-# Debugging
-#_____________________________________________________________
+#_______________________________________________________________________________________
+# Part 2
+# Update the genetic map using the rutgers map using 1000 genome interpolation map
+#_______________________________________________________________________________________
+
+# Update the genetic map
+
 sprintf('Time de lancement : %s', Sys.time())
 snp_physical_positions = "/neurospin/ukb/genetic/GENETIC_DATA_500k/HAPLOTYPES/v2/ukb_hap_"
-rutgers_map = "/neurospin/brainomics/bio_resources/rutgers_map_v3"
-output = '/neurospin/tmp/ymekki/new_repo/gwhap/augmented_genetic_map_approx_function'
-create_augmented_genetic_map(snp_physical_positions=snp_physical_positions, genetic_map_dir=rutgers_map, save_genetic_map=TRUE, output=output)
+m1000_genome_map = "/neurospin/brainomics/bio_resources/hg19_maps/interpolated_maps/1000-genomes-genetic-maps-master/interpolated_from_hapmap"
+output = '/neurospin/tmp/ymekki/new_repo/gwhap/augmented_genetic_map_1000_genome_interpolated'
+create_augmented_genetic_map(snp_physical_positions=snp_physical_positions, genetic_map_dir=m1000_genome_map, map_name='1000_genome_interpolated', save_genetic_map=TRUE, output=output)
 sprintf('Time de fin : %s', Sys.time())
 
 # Read the genetic map -not mine-
@@ -44,5 +40,6 @@ load("/neurospin/brainomics/2019_gwhap/data/genMap_all.Rdata")
 
 # read the augmented genetic map
 # localy
-augmented_genetic_map_dir = '/neurospin/tmp/ymekki/new_repo/gwhap/augmented_genetic_map_approx_function'
+augmented_genetic_map_dir = '/neurospin/tmp/ymekki/new_repo/gwhap/augmented_genetic_map_1000_genome_interpolate'
 augmented_map_df = get_augmented_genetic_map(augmented_genetic_map_dir, chromosomes=1:23)
+
