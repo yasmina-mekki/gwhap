33b651c68cd44b5ae1575d51a3b1aba2320792d5
Yasmina MEKKI <yas.mekki@gmail.com>
2020-03-10 18:55:04 +0100

    read 100 genome interpolated map and 100 genome map

--- R/utils.R
+++ R/utils.R
@@ -10,11 +10,51 @@
 #' @param file_path the location of the rutgers map
 #'
 #' @return the rutgers map in a data.table structure
-#' @import readr
+#' @import data.table
 #' @export
+#' 
 get_rutgers_map <- function(file_path){
-  return(suppressMessages(fread(file_path)))
+  chr_rutgers_map = suppressMessages(fread(file_path))
+  return(data.frame(rsid = chr_rutgers_map$Marker_name,
+                    position = chr_rutgers_map$Build37_start_physical_position,
+                    cM = chr_rutgers_map$Sex_averaged_start_map_position))
 }
+
+#' get 1000 genome interpolated map
+#'
+#' @param filepath the location of the 1000 genome interpolated map
+#'
+#' @return the 1000 genome interpolated map in a data.table structure
+#' @import data.table
+#' @import R.utils
+#' @export
+#' 
+get_1000_genome_interpolated_map <- function(file_path){
+  chr_1000_genome_interpolated_map = suppressMessages(fread(file_path))
+  chr_1000_genome_interpolated_map_filtred = data.frame(rsid = chr_1000_genome_interpolated_map[, 1],
+                                                        position = chr_1000_genome_interpolated_map[, 2],
+                                                        cM = chr_1000_genome_interpolated_map[, 3])
+  colnames(chr_1000_genome_interpolated_map_filtred) = c('rsid', 'position', 'cM')
+  return(chr_1000_genome_interpolated_map_filtred)
+}
+
+
+#' get 1000 genome map
+#'
+#' @param file_path the location of the 1000 genome map
+#'
+#' @return the 1000 genome map in a data.table structure
+#' @import data.table
+#' @export
+#'
+#' @examples
+get_1000_genome_map <- function(file_path){
+  chr_1000_genome_map = suppressMessages(fread(file_path))
+  return(data.frame(position = chr_1000_genome_map$position,
+                    cM = chr_1000_genome_map$Genetic_Map(cM)))
+}
+
+
 
 #' read the genetic map
 #'
@@ -24,7 +64,6 @@ get_rutgers_map <- function(file_path){
 #' @return List representing the genetic map loaded.
 #' @export
 #'
-#' @examples
 get_genetic_map <- function(genetic_map_dir, chromosomes=1:23) {
 
   gen_map = list()
