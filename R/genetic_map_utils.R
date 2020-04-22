dbe075655e5fa1bee425bcbbcfd498e80157bd76
Vincent Frouin <vincent.frouin@cea.fr>
2020-04-22 14:42:47 +0200

    ADD: full integration of S4 Genetic_Map read with know format of genetic_map

--- R/genetic_map_utils.R
+++ R/genetic_map_utils.R
@@ -1,9 +1,20 @@
 # This class was created followingthe principled description from:
 # https://www.cyclismo.org/tutorial/R/s4Classes.html#creating-an-s4-class
+# Documentation and NAMESPACE : http://r-pkgs.had.co.nz/man.html#man-classes
+# See examples : https://github.com/ropensci/datapack/blob/master/R/DataObject.R
 
 # Create the base Genetic_Map S4 class
 #
 # This is used to represent the Genetic_Map.
+
+#' An S4 class to represent a Genetic_Map.
+#'
+#' @name Genetic_Map
+#' @rdname Genetic_Map
+#' @slot filepaths A filepath character vector
+#' @slot encodings A Named list c('cM', 'position', 'chr')
+#' @slot gmapData A data.frame for the read genetic map data
+#' @export Genetic_Map
 Genetic_Map <- setClass(
     # Set the name for the class
     "Genetic_Map",
@@ -12,14 +23,14 @@ Genetic_Map <- setClass(
     slots = c(
             filepaths = "character",
             encodings = "list",
-            gmap = "data.frame"
+            gmapData = "data.frame"
             ),
 
     # Set the default values for the slots. (optional)
     prototype=list(
             filepaths = c(),
-            encodings   = list("chr"=NULL, "cM"=NULL, "pos"=NULL),
-            gmap = data.frame()
+            encodings   = list("chr"=NULL, "cM"=NULL, "position"=NULL),
+            gmapData = data.frame()
             ),
 
     # Make a function that can test to see if the data is consistent.
@@ -34,8 +45,9 @@ Genetic_Map <- setClass(
         if(!is.list(object@encodings)) {
             return("The encodings should be a list")
         }
-        if(!all(names(object@encodings) %in% c("chr", "cM", "pos"))) {
-            return("The encodings list should contain chr, cM and pos keys")
+        if(!all(names(object@encodings) %in% c("chr", "cM", "position"))) {
+            print(names(object@encodings))
+            return("The encodings list should contain chr, cM and position keys")
         }
         # specific encodings@chr validity checks
         if (!(is.list(object@encodings[["chr"]]) | is.character(object@encodings[["chr"]]))) {
@@ -56,11 +68,32 @@ Genetic_Map <- setClass(
 # create a method to read the Genetic_Map map
 # first define hidden ancillary functions
 .read_gmap_no_internal_chrom_info <- function(object) {
-    print(object@filepaths)
-    return(data.frame(c("k")))
+    outdf = data.frame()
+    for (key in names(object@encodings$chr)) {
+        # key in this map (filename <-> #chr)
+        chr_num = object@encodings$chr[[key]]
+        df = fread(key)
+        # get the sepecified column and fix names - robust code to keep
+        col_tokeep = names(encodings)[names(encodings) %in% c('cM', 'position')]
+        col_toextract = sapply(col_tokeep, function(s) object@encodings[[s]])
+        df = df[, ..col_toextract]
+        colnames(df) = col_tokeep
+        # set the chr number
+        df[, 'chr'] = chr_num
+        # collate w/ existing
+        outdf = rbind(outdf, df)
+    }
+
+    return(outdf)
 }
 
+
 # now define the exposet object Genetic_Map methods
+#' readData perform the actual reading of the genetic maps data based on the encodings
+#' @name readData
+#' 
+#' @aliases readData
+#' @export
 setGeneric(name="readData",
     def=function(object)
     {
@@ -68,40 +101,24 @@ setGeneric(name="readData",
     }
 )
 
+#' @rdname readData
+#' @return data for map based on mutiple chromfiles
+#' @aliases getData
+#' @examples
+#' TOBEFIXED
 setMethod(f="readData",
     signature="Genetic_Map",
     definition=function(object)
     {
         if (is.list(object@encodings[["chr"]])) {
         # chr information not in the files
-            object@gmap = .read_gmap_no_internal_chrom_info(object)
+            object@gmapData = .read_gmap_no_internal_chrom_info(object)
         }
         else {
         # chr information is in the files
-            object@gmap = data.frame()
+            object@gmapData = data.frame()
         }
         return(object)
     }
 )
 
-filepaths = c(system.file("extdata", "chr1.interpolated_genetic_map.gz",
-                          package="gwhap", mustWork=TRUE),
-              system.file("extdata", "chr2.interpolated_genetic_map.gz",
-                          package="gwhap", mustWork=TRUE))
-chr = list(1, 2)
-names(chr) = filepaths
-encodings = list("cM"="cM",
-                 "pos"="bp",
-                 "chr"=chr
-                )
-a = Genetic_Map(filepaths=filepaths, encodings=encodings)
-print(a)
-a = readData(a)
-print(a)
-
-#~ # example to test:
-#~ sas internal
-#~ /neurospin//brainomics/bio_resources/hg19_maps/1000GP_Phase3_b37/genetic_map_chr21_combined_b37.txt
-#~ /neurospin//brainomics/bio_resources/hg19_maps/interpolated_maps/1000-genomes-genetic-maps-master/interpolated_from_hapmap/chr21.interpolated_genetic_map.gz
-#~ /neurospin//brainomics/bio_resources/rutgers_map_v3
-# avec internal carte bgen bgi ?
--- inst/example/genetic_map_read.R
+++ inst/example/genetic_map_read.R
@@ -1,17 +1,98 @@
 library(gwhap)
 
-# read 1000 genome interpolated
+########################################################################
+# This R scripts contains example to read genetic map
+# object Genetic_Map
+
+########################################################################
+# ::TOY:: read 1000 genome interpolated
+
+# give the list of files to consider
+f1 = system.file("extdata", "chr1.interpolated_genetic_map.gz",
+                          package="gwhap", mustWork=TRUE)
+f2 = system.file("extdata", "chr2.interpolated_genetic_map.gz",
+                          package="gwhap", mustWork=TRUE)
+# build a NAMED list mapping the chrom information
+chr = list(1, 2)
+names(chr) = c(f1, f2)
+
+# now build the parameter for Genetic_Map object. Inspect the file to
+# assign the right column name for cM and position.
+# Here the chrom info is not read from the file it is read from the
+# NAMED list chr
+filepaths = c(f1, f2)
+encodings = list("cM"="cM", "position"="bp","chr"=chr)
+
+# get an instance of Genetic_Map
+genetic_map = Genetic_Map(filepaths=filepaths, encodings=encodings)
+print(genetic_map)
+
+# finally read the corresponding data and have a quick look at it
+genetic_map = readData(genetic_map)
+print(genetic_map)
+
+
+########################################################################
+# ::ACTUAL DATA:: read 1000 genome interpolated
+#~ /neurospin//brainomics/bio_resources/hg19_maps/interpolated_maps/1000-genomes-genetic-maps-master/interpolated_from_hapmap/chr21.interpolated_genetic_map.gz
 filepaths
 encodings
 genetic_map
 
-# read 1000 genome
-filetpaths
-encodings
-genetic_map
 
-# read Rutgers
-filetpaths
-encodings
-genetic_map
+########################################################################
+# ::ACTUAL DATA:: read 1000 genome
+#~ /neurospin//brainomics/bio_resources/hg19_maps/1000GP_Phase3_b37/genetic_map_chr21_combined_b37.txt
+# filter on the desired columns : position COMBINED_rate(cM/Mb)
+#                                 Genetic_Map(cM)
 
+# give the list of files to consider (here file for chr21 and chr22
+NS_ROOT = '/neurospin//brainomics/bio_resources/hg19_maps/1000GP_Phase3_b37'
+f1 = file.path(NS_ROOT, 'genetic_map_chr21_combined_b37.txt')
+f2 = file.path(NS_ROOT, 'genetic_map_chr22_combined_b37.txt')
+
+# build a NAMED list mapping the chrom information
+chr = list(21, 22)
+names(chr) = c(f1, f2)
+
+# now build the parameter for Genetic_Map object.
+# the columns in the file are : 'position', 'COMBINED_rate(cM/Mb)', 'Genetic_Map(cM)'
+filepaths = c(f1, f2)
+encodings = list("cM"="Genetic_Map(cM)",
+                 "position"="position",
+                 "chr"=chr)
+
+# get an instance of Genetic_Map
+genetic_map = Genetic_Map(filepaths=filepaths, encodings=encodings)
+print(genetic_map)
+
+# finally read the corresponding data and have a quick look at it
+genetic_map = readData(genetic_map)
+print(genetic_map)
+
+
+
+########################################################################
+# ::ACTUAL DATA:: read Rutgers
+# filter on the desired columns : Sex_averaged_start_map_position, 
+#                                 Build37_start_physical_positio,
+#                                 Marker_name 
+#                                 chromosome code
+NS_ROOT = '/neurospin//brainomics/bio_resources/rutgers_map_v3'
+filelist=sapply(21:22, function(i) file.path(NS_ROOT,sprintf("RUMapv3_B137_chr%d.txt", i)))
+chr=lapply(21:22, function(i) i)
+names(chr) = filelist
+
+filepaths=filelist
+encodings = list("cM"="Sex_averaged_start_map_position",
+                 "position"="Build37_start_physical_position",
+                 "chr"=chr)
+
+# get an instance of Genetic_Map
+genetic_map = Genetic_Map(filepaths=filepaths, encodings=encodings)
+print(genetic_map)
+
+# finally read the corresponding data and have a quick look at it
+genetic_map = readData(genetic_map)
+print(genetic_map)
+print(dim(genetic_map@gmapData))
